f92e5ec017d673fd17547cef3576487bd521b43e
Alex MDC <zedokto12@hotmail.com>
2023-06-03 14:56:36 -0600

    works at simple type

--- parser_1.py
+++ parser_1.py
@@ -350,7 +350,7 @@ def p_class(p):
     class : CLASS ID set_class_scope LCURL class4 class2 function class3 RCURL SEMIC
     '''
     
-    p[0] = ('class',p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9],p[10],p[11])
+    p[0] = ('class',p[1],p[2],p[3],p[4],p[5],p[6],p[7],p[8],p[9])
 
 def p_class4(p):
      '''
